f8748a3e9d68517e6e3ba10d1aba7d94b4cd9ce2
Omar <omartsadek@gmail.com>
2022-03-28 01:15:21 -0400

    commit tree 09c2b103da06fee35d0f377326a2c6ebba812fbd

---
+++ index.js
@@ -0,0 +1,27 @@
+function shout(string){
+    return string.toUpperCase();
+}
+function whisper(string){
+    return string.toLowerCase();
+}
+function logShout(string){
+    console.log(shout(string))
+}
+function logWhisper(string){
+    console.log(whisper(string))
+}
+function sayHiToHeadphonedRoommate(string){
+    sayHiToHeadphonedRoommate=("I can't hear you!");
+}
+
+
+   
+ 
+
+    
+
+
+
+
+
+
